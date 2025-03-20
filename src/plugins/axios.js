1bf97db7fb241678806950ecf74fe1152c301df8
djimenezjerez <djimenezjerez@gmail.com>
2025-03-19 22:40:46 -0400

    Cambio de servidor apuntado a localhost

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,11 +3,11 @@ import axios from 'axios'
 const app = JSON.parse(localStorage.getItem('app'))
 
 let axiosInstance = axios.create({
-  withCredentials: true,
   baseURL: import.meta.env.VITE_APP_URL,
   headers: {
+    'Access-Control-Allow-Credentials': true,
     'Content-Type': 'application/json; charset=UTF-8',
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'localhost:3000',
     'Authorization': `Bearer ${app && app.token}`,
   },
 })
